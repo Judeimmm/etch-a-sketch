98c3749314a7d17760fe581b9b75225c349edd58
Judeimmm <23743374+Judeimmm@users.noreply.github.com>
2023-09-04 12:57:46 -0500

    add html boilerplate and div creation

---
+++ script.js
@@ -0,0 +1,7 @@
+
+const container = document.querySelector('#container');
+for (i = 1 ; i <= 256; i++){
+    let divs = document.createElement('div');
+    divs.className = 'grid';
+    document.getElementById('container').appendChild(divs);
+};
