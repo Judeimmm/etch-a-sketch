3323825516aa506f4b26e04d9628502afbf46857
Judeimmm <23743374+Judeimmm@users.noreply.github.com>
2023-09-05 14:07:08 -0500

    Organize divs into a 16x16 square

--- script.js
+++ script.js
@@ -4,4 +4,5 @@ for (i = 1 ; i <= 256; i++){
     let divs = document.createElement('div');
     divs.className = 'grid';
     document.getElementById('container').appendChild(divs);
+    divs.textContent = ""
 };
