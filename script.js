63821af4a98eb2f0a3876db74be9b17076f84168
Judeimmm <23743374+Judeimmm@users.noreply.github.com>
2023-09-07 00:34:27 -0500

    Add feature to color divs

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 
 const container = document.querySelector('#container');
 for (i = 1 ; i <= 256; i++){
-    let divs = document.createElement('div');
+    const divs = document.createElement('div');
     divs.className = 'grid';
     document.getElementById('container').appendChild(divs);
-    divs.textContent = ""
+    divs.textContent = "";
 };
+
+const div = document.querySelectorAll(".grid");
+div.forEach(div => div.addEventListener("click", () => {
+    div.classList.add("black");
+})
+);
