643b75b5b1d4f5295456457b56115f0753faf9ad
Judeimmm <23743374+Judeimmm@users.noreply.github.com>
2023-09-08 23:12:32 -0500

    Add working reset button

--- script.js
+++ script.js
@@ -1,22 +1,32 @@
-
+let gridResolution = 256;
 const container = document.querySelector('#container');
-for (i = 1 ; i <= 256; i++){
+function defaultGrid() {
+for (i = 1 ; i <= gridResolution; i++){
     const divs = document.createElement('div');
     divs.className = 'grid';
     document.getElementById('container').appendChild(divs);
     divs.textContent = "";
-};
-
-const div = document.querySelectorAll(".grid");
-div.forEach(div => div.addEventListener("mouseover", () => {
-    div.classList.add("black");
-})
-);
-function getGridResolution() {
-    let gridResolution = parseInt(prompt("What resolution do you want the grid?", 16));
-    if (isNaN(gridResolution) == true || gridResolution > 100) return alert("Error!");
-    console.log(gridResolution)
-    return gridResolution;
 }
+    const div = document.querySelectorAll(".grid");
+    div.forEach(div => div.addEventListener("click", () => {
+        div.classList.add("black");
+})
+)};
+
+defaultGrid()
 const resetGridSizeButton = document.querySelector(".reset")
-resetGridSizeButton.addEventListener("click", getGridResolution)
+
+resetGridSizeButton.addEventListener("click", () => {
+let gridResolution;
+container.textContent = ' '
+gridResolution = parseInt(prompt("What resolution do you want the grid?", 16)); if (isNaN(gridResolution) == true || gridResolution > 100) return alert("Error!");
+for (i = 1 ; i <= Math.pow(gridResolution, 2); i++){
+    const divs = document.createElement('div');
+    divs.className = 'grid'
+    document.getElementById('container').appendChild(divs);
+    divs.textContent = "";
+    elementPadding = 22 - (gridResolution - 16);
+    divs.style.padding = `${elementPadding}px`; 
+};
+})
+
